6325b2f43ad4bece48412817ec5a8d135adec2fe
Daniel-Roch <daniel_roch4@hotmail.com>
2026-09-06 17:44:17 -0300

    feat: return role in schema - to-do: remove this

--- src/schemas/catalog.schema.ts
+++ src/schemas/catalog.schema.ts
@@ -22,6 +22,12 @@ export const createUserSchema = z.object({
     .min(1, "Campo obrigatório não informado: username"),
   password: z.string().min(1, "Campo obrigatório não informado: password"),
   email: z.email({ message: "Email inválido" }).trim(),
+  role: z
+    .enum(USER_ROLES, {
+      message: "Role inválido. Valores permitidos: PROFESSOR, ALUNO",
+    })
+    .optional()
+    .default("ALUNO"),
   mobilePhone: z.string().trim().optional(),
   externalId: z.string().trim().optional(),
   isActive: z.boolean().optional().default(true),
